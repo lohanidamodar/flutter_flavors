5d2e4a7fbe39d4eecd4bd1165a41412af544f41c
Damodar Lohani <dlohani48@gmail.com>
2019-04-13 10:27:30 +0545

    adding test data to firestore

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../../resources/app_config.dart';
 
@@ -8,6 +9,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool loading = false;
+  TextEditingController _controller = TextEditingController();
 
   void _incrementCounter() {
     setState(() {
@@ -33,6 +36,14 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
+            loading ? CircularProgressIndicator() : Container(),
+            TextField(
+              controller: _controller,
+            ),
+            RaisedButton(
+              child: Text("Add to Firestore"),
+              onPressed: _addToFirestore,
+            )
           ],
         ),
       ),
@@ -42,5 +53,17 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Icon(Icons.add),
       ), 
     );
+  }
+
+  _addToFirestore() async {
+    if(_controller.text.isEmpty) return;
+    setState(() {
+      loading = true;
+    });
+    await Firestore.instance.collection('mycoll').add({"string": _controller.text});
+    _controller.text = "";
+    setState(() {
+      loading = false;
+    });
   }
 }
